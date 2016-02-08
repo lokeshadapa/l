eb14ec20908b13bae6f24ff41d1e56ef64990bde
lokeshadapa <lokeshadapa1231@gmail.com>
2016-02-08 13:55:56 +0530

    Create pipe.c

---
+++ pipe.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+
+int main(int argc,char *argv[])
+{
+	printf("%s","popen");
+}
